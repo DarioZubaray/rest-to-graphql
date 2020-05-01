fc8dda081e5fbca489339732ada1982b98e81d62
Dariojz <dzubaray@gmail.com>
2020-05-01 17:20:16 -0300

    Agregando las consultas de circuitos

---
+++ src/data/data-circuits.ts
@@ -0,0 +1,27 @@
+import { F1 } from './data-source';
+
+export class CircuitsData extends F1 {
+    constructor() {
+        super();
+    }
+
+    async getCircuits(pageElements: number = -1, page: number = 1) {
+        if (pageElements === -1) {
+            return await this.get('circuits.json?limit=10000', {
+                cacheOptions: { ttl: 60 }
+            });
+        }
+        const offset = (page -1 ) * pageElements;
+        const limit = pageElements;
+
+        return await this.get(`circuits.json?limit=${ limit }&offset=${ offset }`, {
+            cacheOptions: { ttl: 60 }
+        });
+    }
+
+    async getCircuit(id: string) {
+        return await this.get(`circuits/${id}.json`, {
+            cacheOptions: { ttl: 60 }
+        });
+    }
+}
